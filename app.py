2d066086e52b878d4b102589e6b58ac80ba60107
NandoAACF <nando.aacf88@gmail.com>
2023-07-05 20:33:11 +0700

    feat: add model information page

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 from predict_page import show_predict_page
 from insight_page import show_insight_page
+from model_information_page import show_model_information_page
 
-page = st.sidebar.selectbox("Predict Or Insight", ("Predict", "Insight"))
+page = st.sidebar.selectbox("Predict Or Insight", ("Predict", "Insight", "Model Information"))
 
 if page == 'Predict':
     show_predict_page()
-else:
+elif page == 'Insight':
     show_insight_page()
+elif page == 'Model Information':
+    show_model_information_page()
